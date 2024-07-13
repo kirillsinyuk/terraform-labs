13302bbd66675a33bceef9988de9f7a683e2b797
ksiniuk <kirill.s.works@gmail.com>
2024-07-13 10:02:05 +0400

    19 add vpc outputs

--- section2/main.tf
+++ section2/main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "vpc" {
     Name        = var.vpc_name
     Environment = "demo_environment"
     Terraform   = "true"
-    Region = data.aws_region.current.name
+    Region      = data.aws_region.current.name
   }
 }
 
